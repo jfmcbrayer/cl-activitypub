13dc75262a2090d600d50bb5fe055e03dad50bdb
Jason McBrayer <jmcbray@carcosa.net>
2017-06-26 12:19:15 -0400

    Fix some name conflicts

--- clap-activities.lisp
+++ clap-activities.lisp
@@ -1,8 +1,18 @@
 (defpackage :CLAP.ACTIVITIES
   (:use :CL :CL-JSON :CLAP.CORE)
   (:export accept tentative-accept
-           add arrive
-           create))
+           invite reject tentative-reject
+           travel arrive
+           create delete-activity
+           undo update
+           add remove-activity move
+           follow ignore-activity
+           like announce
+           block-activity flag
+           dislike
+           join leave
+           view listen-activity read-activity
+           question))
 (in-package :CLAP.ACTIVITIES)
 
 (defclass accept (clap.core:activity) ())
@@ -15,19 +25,19 @@
 (defclass arrive (clap.core:intransitive-activity) ())
 
 (defclass create (clap.core:activity) ())
-(defclass delete (clap.core:activity) ())
+(defclass delete-activity (clap.core:activity) ())
 (defclass undo (clap.core:activity) ())
 (defclass update (clap.core:activity) ())
 
 (defclass add (clap.core:activity) ())
-(defclass remove (clap.core:activity) ())
+(defclass remove-activity (clap.core:activity) ())
 (defclass move (clap.core:activity) ())
 
 (defclass follow (clap.core:activity) ())
-(defclass ignore (clap.core:activity) ())
+(defclass ignore-activity (clap.core:activity) ())
 (defclass like (clap.core:activity) ())
 (defclass announce (clap.core:activity) ())
-(defclass block (ignore) ())
+(defclass block-activity (ignore) ())
 (defclass flag (clap.core:activity) ())
 (defclass dislike (clap.core:activity) ())
 
@@ -35,11 +45,11 @@
 (defclass leave (clap.core:activity) ())
 
 (defclass view (clap.core:activity) ())
-(defclass listen (clap.core:activity) ())
-(defclass read (clap.core:activity) ())
+(defclass listen-activity (clap.core:activity) ())
+(defclass read-activity (clap.core:activity) ())
 
 (defclass question (clap.core:intransitive-activity)
-  (one-of :accessor one-of :initarg :one-of)
-  (any-of :accessor any-of :initarg :any-of)
-  (closed :accessor closed :initarg :closed))
+  ((one-of :accessor one-of :initarg :one-of)
+   (any-of :accessor any-of :initarg :any-of)
+   (closed :accessor closed :initarg :closed)))
 
