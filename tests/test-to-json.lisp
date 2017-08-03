3a2a36b562f4a236ac7d86101753d213531eda4e
Jason McBrayer <jmcbray@cassilda.carcosa.net>
2017-08-03 07:41:10 -0400

    Add slightly more realistic test

--- tests/test-to-json.lisp
+++ tests/test-to-json.lisp
@@ -1,14 +1,39 @@
 (in-package "clap-tests")
 
-(defvar *my-object* (make-instance 'clap-entities:note
+(defvar *basic-object* (make-instance 'clap-entities:note
                                    :id "https://example.com/obs/foo"
                                    :image "https://example.com/images/foo"
                                    :content "This is a note."
                                    :to '("https://example.com/PUBLIC")))
+(defparameter *activity-object*
+  (make-instance 'clap-entities:activity
+                 :summary "Sally did something to a note"
+                 :actor (make-instance 'clap-entities:person
+                                       :name "Sally")
+                 :object (make-instance 'clap-entities:note
+                                        :name "A Note")))
+(defparameter *activity-json-string*
+  "{
+  \"@context\":\"https://www.w3e.org/ns/activitystreams\",
+  \"type\":\"Activity\",
+  \"summary\":\"Sally did something to a note\",
+  \"object\":
+  {
+    \"@context\":\"https://www.w3e.org/ns/activitystreams\",
+    \"type\":\"Note\",
+    \"name\":\"A Note\"
+  },
+  \"actor\":
+  {
+    \"@context\":\"https://www.w3e.org/ns/activitystreams\",
+    \"type\":\"Person\",
+    \"name\":\"Sally\"
+  }
+}")
 
-(plan 1)
+(plan 2)
 
-(is (clap-entities:as-json-string *my-object*)
+(is (clap-entities:as-json-string *basic-object*)
     "{
   \"@context\":\"https://www.w3e.org/ns/activitystreams\",
   \"type\":\"Note\",
@@ -16,5 +41,7 @@
   \"image\":\"https://example.com/images/foo\",
   \"to\":[\"https://example.com/PUBLIC\"]
 }")
+
+(is (clap-entities:as-json-string *activity-object*) *activity-json-string*)
 
 (finalize)
